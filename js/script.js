771fc8feb46b844af8951e06545cb44086c35c72
Everton Lima <ever-tonlima@hotmail.com>
2016-06-16 15:14:06 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+ var video= document.querySelectorAll(".video");
+	 	 var close= document.querySelector(".close");
+	 	 var caption= document.querySelector(".caption");
+
+	 	 for(var i= 0; i < video.length; i++) {
+	 	 	video[i].addEventListener("click", function(event) {
+	 	 		event.preventDefault();
+	 	 		document.querySelector(".modal-video").src= this.href;
+	 	 		document.querySelector(".modal").style.display= "block";
+	 	 		caption.innerHTML= this.children[1].innerHTML + " - " + this.children[2].innerHTML;
+	 	 	})
+	 	 }
+
+close.addEventListener("click", function() {
+	 document.querySelector(".modal").style.display= "none";
+})

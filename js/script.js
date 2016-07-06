24d611ea70fe093eaa1bb859a51cb2b63fd8a415
Everton Lima <ever-tonlima@hotmail.com>
2016-07-05 22:08:55 -0300

    Corrigindo mobile e adicionando footer

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 	 	 for(var i= 0; i < video.length; i++) {
 	 	 	video[i].addEventListener("click", function(event) {
 	 	 		event.preventDefault();
-	 	 		document.querySelector(".modal-video").src= this.href;
+	 	 		document.querySelector(".modal-video").src= this.href + "?autoplay=1";
 	 	 		document.querySelector(".modal").style.display= "block";
 	 	 		caption.innerHTML= this.children[1].innerHTML + " - " + this.children[2].innerHTML;
 	 	 	})
